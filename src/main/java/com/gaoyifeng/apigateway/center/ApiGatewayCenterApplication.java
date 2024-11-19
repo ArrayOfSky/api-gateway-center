6b3bff034aecfe1b6cc5756a80e532637e1962e4
Gaoyifeng <104308413+ArrayOfSky@users.noreply.github.com>
2024-11-19 19:28:05 +0800

    init project

---
+++ src/main/java/com/gaoyifeng/apigateway/center/ApiGatewayCenterApplication.java
@@ -0,0 +1,14 @@
+package com.gaoyifeng.apigateway.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ApiGatewayCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayCenterApplication.class, args);
+    }
+
+}
